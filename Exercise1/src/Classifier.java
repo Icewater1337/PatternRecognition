2068a05cf1b3f104b3c7b0591a879ba267478012
Icewater1337 <fuchsmath@gmail.com>
2017-02-21 01:44:24 +0100

    initial almost working commit, superslow, fix on the way

--- Exercise1/src/Classifier.java
+++ Exercise1/src/Classifier.java
@@ -1,9 +1,11 @@
 import java.io.*;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
+
+import static java.util.Map.Entry.comparingByValue;
+import static java.util.stream.Collectors.counting;
+import static java.util.stream.Collectors.groupingBy;
 
 /**
  * Created by Icewater on 20.02.2017.
@@ -12,25 +14,103 @@ import java.util.stream.Stream;
 public class Classifier {
 
     public static void main(String[] args) throws FileNotFoundException {
+        long startTime = System.currentTimeMillis();
 
         ArrayList<Number> training = new ArrayList<Number>();
         ArrayList<Number> testing = new ArrayList<Number>();
 
         String trainPath = "C:\\Users\\Icewater\\IdeaProjects\\PatternRecognition\\train.csv";
         String testPath = "C:\\Users\\Icewater\\IdeaProjects\\PatternRecognition\\test.csv";
-        ArrayList<int[]> train = loadCsv(trainPath);
-        ArrayList<int[]> test = loadCsv(testPath);
+        List<int[]> train = loadCsv(trainPath);
+        List<int[]> test = loadCsv(testPath);
+
+        train =  train.subList(0,3000);
+        test = test.subList(0,2000);
 
         training= createObjects(train);
         testing = createObjects(test);
 
+        ArrayList<List<Distance>> manhattanKnn = new ArrayList<>();
+        ArrayList<List<Distance>> euclidKnn = new ArrayList<>();
 
 
+        int[] k = {1,3,5,10,15};
+        // run for all test numbers
+        for ( int i : k) {
+            KNN knn = new KNN();
 
-        KNN knn = new KNN();
+            for ( Number nbr : testing) {
+                manhattanKnn.add(knn.getKNN("Manhattan",i,training,nbr));
+                euclidKnn.add(knn.getKNN("Euclid",i,training,nbr));
+            }
+            ArrayList<ResultNode> resultsManhattan;
+            ArrayList<ResultNode> resultsEuclid;
+            resultsManhattan = classify(manhattanKnn);
+            resultsEuclid = classify(euclidKnn);
+
+
+            System.out.println("The accuracy of the Manhattan distance " +i + "-NN was" +getAccuracy(resultsManhattan));
+            System.out.println("The accuracy of the Euclid distance " +i + "-NN was" +getAccuracy(resultsEuclid));
+        }
+
+        long stopTime = System.currentTimeMillis();
+        long timeElapsed = stopTime- startTime;
+        System.out.println("Time elapsed: " + timeElapsed +" which is " + timeElapsed/test.size() +" per test case");
+
+
     }
 
-    private static ArrayList<Number> createObjects(ArrayList<int[]> train) {
+    private static double getAccuracy(ArrayList<ResultNode> results) {
+        int correctIdentifiedDigits = 0;
+        int totalDigits = results.size();
+
+        for ( ResultNode node : results) {
+            if ( node.isClassifiedCorrectly()) {
+                correctIdentifiedDigits++;
+            }
+
+        }
+        return correctIdentifiedDigits/totalDigits;
+    }
+
+    private static ArrayList<ResultNode> classify(ArrayList<List<Distance>> knnsOfNumber) {
+        ArrayList<ResultNode> results = new ArrayList<>();
+
+        for ( List<Distance> numberToClassify :knnsOfNumber ) {
+            Map<Integer, Long> occurrences = numberToClassify.
+                    stream()
+                    .map(Distance::getNeighborClassification)
+                    .collect(groupingBy(p -> p, counting()));
+            Map<Integer, Long> sortedOccurrences = new LinkedHashMap<>();
+
+            // have to get the nearest neighbors ffrom somewhere else oO
+            ArrayList<Number> nearestNeighbors = numberToClassify.stream().map(elt -> elt.getNeighbor()).collect(Collectors.toCollection(ArrayList::new));
+
+            // sort map
+            occurrences.entrySet().stream()
+                    .sorted(Map.Entry.<Integer,Long>comparingByValue().reversed())
+                    .forEachOrdered(x -> sortedOccurrences.put(x.getKey(), x.getValue()));
+
+            Object[] values = sortedOccurrences.values().toArray();
+            Object[] keys =  sortedOccurrences.keySet().toArray();
+            boolean isClassifiedCorrectly = true ? (int)keys[0] == numberToClassify.get(0).getNumber().getClassification() : false;
+                if (sortedOccurrences.size() == 1 ||values[0] == values[1] ) {
+                    // return kmeans 1 for now.
+                    results.add( new ResultNode(numberToClassify.get(0).getNumber(), nearestNeighbors.subList(0,1), (int)keys[0], isClassifiedCorrectly));
+                }
+                else if ((long)values[0] > (long)values[1] ) {
+                    //
+
+                    results.add(new ResultNode(numberToClassify.get(0).getNumber(), nearestNeighbors, (int)keys[0],isClassifiedCorrectly ));
+                }  else
+                    return null;
+
+        }
+
+        return results;
+    }
+
+    private static ArrayList<Number> createObjects(List<int[]> train) {
         ArrayList<Number> nbrs = new ArrayList<Number>();
         for ( int[] elt : train ) {
            // the classification of the Number
--- Exercise1/src/KNN.java
+++ Exercise1/src/KNN.java
@@ -1,34 +1,54 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * Created by Icewater on 20.02.2017.
  */
 public class KNN {
 
-    private List p = new ArrayList();
-    private List x = new ArrayList();
+    private ArrayList<Distance> alreadyCalculatedDistances;
+    private int k;
+    private Number Number;
 
+    public ArrayList<Distance> getAlreadyCalculatedDistances() {
+        return alreadyCalculatedDistances;
+    }
+
+    public int getK() {
+        return k;
+    }
+
+    public Number getNumber() {
+        return Number;
+    }
 
     public KNN() {
 
 
     }
 
+    public static KNN getInstance() {
+        // return the proper instance, so we can avoid to calculate the same number again, when only K changes. (should save lots of time)
+        return null;
+    }
+
 
 
     //Euclidean Distance
-    public double getEuclideanDistance(int[] p, int[] x) {
-
-        int length = Math.min(p.length, x.length);
+    public Distance getEuclideanDistance(Number p, Number x) {
+        ArrayList<Integer> pixelsP = p.getPixels();
+        ArrayList<Integer> pixelsX = x.getPixels();
+        int length = Math.min(pixelsP.size(), pixelsX.size());
         int sum = 0;
 
         for (int i = 0; i < length; i++) {
-            sum += Math.pow(2, (x[i] - p[i]));
+            sum += Math.pow(2, (pixelsX.get(i) - pixelsP.get(i)));
         }
 
-        return Math.sqrt(sum);
+        return new Distance(Math.sqrt(sum),p,x, p.getClassification());
 
 //        return Math.sqrt(Arrays.stream(integers).mapToDouble(i -> Math.pow(2,i)).sum());
 
@@ -36,15 +56,18 @@ public class KNN {
 
 
     // manhattan distance
-    public double getManhattanDistance(int[] p, int[] x) {
-        int length = Math.min(p.length, x.length);
+    public Distance getManhattanDistance(Number p, Number x) {
+        ArrayList<Integer> pixelsP = p.getPixels();
+        ArrayList<Integer> pixelsX = x.getPixels();
+
+        int length =  Math.min(pixelsP.size(), pixelsX.size());
         int sum = 0;
 
         for (int i = 0; i < length; i++) {
-            sum += Math.abs(x[i] - p[i]);
+            sum += Math.abs((pixelsX.get(i) - pixelsP.get(i)));
         }
 
-        return sum;
+        return new Distance(sum,p,x, p.getClassification());
     }
 
     public void classify1NN(ArrayList<int[]> p, ArrayList<int[]> x) {
@@ -52,7 +75,42 @@ public class KNN {
 
     }
 
-    public void classifyKNN(int k, ArrayList<int[]> p, ArrayList<int[]> x) {
+    public List<Distance> getKNN(String distanceMethod, int k, ArrayList<Number> p, Number x) {
+        ArrayList<Distance> distances;
+//        if (alreadyCalculatedDistances == null ) {
+            distances = getDistances(distanceMethod, p, x);
+            // get k neighbors of x in p.
+            distances = distances.stream().sorted((nbr1, nbr2) -> Double.compare(nbr1.getNeighborDistance(), nbr2.getNeighborDistance())).collect(Collectors.toCollection(ArrayList<Distance>::new));
+            alreadyCalculatedDistances = distances;
+
+//        } else {
+//            return alreadyCalculatedDistances.subList(0,k);
+//        }
+        return distances.subList(0, k);
+
 
     }
+
+    private ArrayList<Distance> getDistances(String distanceMethod, ArrayList<Number> p, Number x) {
+        ArrayList<Distance> distances = new ArrayList<>();
+        if ( distanceMethod.equals("Manhattan")) {
+            for ( Number nbr : p ) {
+                distances.add(getManhattanDistance(nbr,x));
+            }
+
+        } else if (distanceMethod.equals("Euclid")) {
+            for ( Number nbr : p ) {
+                distances.add(getEuclideanDistance(nbr, x));
+            }
+
+        }
+
+        return distances;
+    }
+
+
+
+
+
+
 }
