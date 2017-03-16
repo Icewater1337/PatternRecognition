7b9b8eaeaa55fc1f2edb2dbc0f1ac12d96b31b83
Icewater1337 <fuchsmath@gmail.com>
2017-03-16 11:55:49 +0100

    second commit

---
+++ Exercise 2/src/KMeans.java
@@ -0,0 +1,36 @@
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Icewater on 13.03.2017.
+ */
+public class KMeans {
+
+
+    public void kmeansClustering (ArrayList<Number> training, int k) throws FileNotFoundException {
+    /*
+    Ensure: clustering C= {C1,....,Ck}
+    1. choose K initial cluster centers m1,....m,k
+    2. repeat
+        assign every xi to the cluster with the nearest center mj
+        recompute mj for each cluster
+    5. until termination criterion
+    6. (optional post processing)
+
+     */
+
+
+
+
+
+
+    }
+
+
+    // calculate the intial clusters. to do this take the two individuals which are the furthest apart.
+    public ArrayList<Number> initialClusters (ArrayList<Number> trainingSet) {
+
+        return null;
+    }
+}
